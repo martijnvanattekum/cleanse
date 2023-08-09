60d7ba83cf9b9b13187af1d936bb3a67049e83d0
Martijn <martijn@Martijns-MacBook-Pro.local>
2023-08-09 17:43:17 +0200

    Added tidy row and coldata getters and setters

--- R/user_functions.R
+++ R/user_functions.R
@@ -329,3 +329,111 @@ drop_metadata <- function(se){
                                              colData = coldt, 
                                              rowData = rowdt)
 }
+
+
+#' Gets column data from an se as a tibble
+#' 
+#' @param se SummarizedExperiment
+#' @examples
+#' get_col_data(seq_se)
+#' @importFrom tibble tibble
+#' @export
+get_col_data <- function(se) {
+  
+  se %>% 
+    SummarizedExperiment::colData() %>% 
+    data.frame %>% 
+    tibble::tibble()
+  
+}
+
+
+#' Gets row data from an se as a tibble
+#' 
+#' @param se SummarizedExperiment
+#' @examples
+#' get_row_data(seq_se)
+#' @importFrom tibble tibble
+#' @export
+get_row_data <- function(se) {
+  
+  se %>% 
+    SummarizedExperiment::rowData() %>% 
+    data.frame %>% 
+    tibble::tibble()
+  
+}
+
+
+#' Sets column data from an se
+#' 
+#' As this does not require inplace operations, this can typically be used
+#' in a pipe
+#' 
+#' @param se SummarizedExperiment
+#' @param coldata data frame or tibble containing the coldata
+#' @examples
+#' new_cd <- get_col_data(seq_se) %>% tidyr::unite(time_treatment, c(time, treatment))
+#' se_new_coldata <- set_col_data(seq_se, new_cd)
+#' @importFrom SummarizedExperiment colData
+#' @importFrom S4Vectors DataFrame
+#' @export
+set_col_data <- function(se, coldata) {
+  
+  se_copy <- se
+  coldata_for_se <- coldata %>% 
+    data.frame() %>% 
+    S4Vectors::DataFrame()
+  
+  SummarizedExperiment::colData(se_copy) <- coldata_for_se
+  se_copy
+  
+}
+
+
+#' Sets row data from an se
+#' 
+#' As this does not require inplace operations, this can typically be used
+#' in a pipe
+#' 
+#' @param se SummarizedExperiment
+#' @param rowdata data frame or tibble containing the coldata
+#' @examples
+#' new_rd <- get_row_data(seq_se) %>% tidyr::unite(gene_group_name, c(gene_group, gene_name))
+#' se_new_rowdata <- set_row_data(seq_se, new_rd)
+#' @importFrom SummarizedExperiment rowData
+#' @importFrom S4Vectors DataFrame
+#' @export
+set_row_data <- function(se, rowdata) {
+  
+  se_copy <- se
+  rowdata_for_se <- rowdata %>% 
+    data.frame() %>% 
+    S4Vectors::DataFrame()
+  
+  SummarizedExperiment::rowData(se_copy) <- rowdata_for_se
+  se_copy
+  
+}
+
+
+#' Gets available options for a particular column of the coldata of an se
+#' 
+#' @param se SummarizedExperiment
+#' @param colname the column name (character) to extract options for
+#' @examples
+#' options_from_coldata(seq_se, "site")
+#' @importFrom dplyr pull
+#' @importFrom stats na.omit
+#' @export
+options_from_coldata <- function(se, colname) {
+  
+  se %>% 
+    get_col_data() %>% 
+    dplyr::pull(!!colname) %>% 
+    unique %>% 
+    stats::na.omit() %>% 
+    as.vector
+  
+}
+
